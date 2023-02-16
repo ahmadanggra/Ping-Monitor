e5ab54a0ceb66df1cc72fc898f87f848b5064497
Ahmad Anggra <ahmadanggra@gmail.com>
2023-02-17 03:14:19 +0700

    done

--- icmp_monitor.py
+++ icmp_monitor.py
@@ -1,10 +1,11 @@
 from time import sleep
 from sys import argv,exit
 from os import system
-from icmplib import ping,exceptions
+from icmplib import ping,exceptions,utils
 from tabulate import tabulate
 from socket import inet_aton
 from re import match
+from socket import gethostbyname,error
 
 # usage icmp_monitor <<update in second>> << list of ip address separate by comma (,)
 # example python icmp_monitor.py 192.168.0.1,192.168.0.2
@@ -13,35 +14,57 @@ def icmp_help():
     print('python icmp_monitor.py [update ping in second] [list of ip address/hostname separated by comma.]')
     exit()    
 
+def hostname_resolves(hostname):
+    try:
+        gethostbyname(hostname)
+        return 1
+    except error:
+        return 0
+
 def main():
-    #hosts = argv[1:]
     if argv[1] == '--help' or argv[1] == '-h':
         icmp_help()
     addresses = list(map(str, argv[-1].split(',')))
-    print(addresses)
     head = ['Host Address', 'Status', 'RTT']
     table = list()
     row = list()
     try:
         while(True):
             system('cls')
-            for address in addresses:
-                host = ping(address, count = 1, timeout = 1)
-                test_ip = match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", address)
-                if test_ip:
+            for address in addresses:              
+                if utils.is_ipv4_address(address):
+                    host = ping(address, count = 1, timeout = 1)
                     row.append(host.address)
+                    if host.is_alive:
+                        row.append('Alive')
+                        row.append(str(host.max_rtt) + ' ms')
+                        table.append(list(row))
+                        row.clear()
+                    else:
+                        row.append('Down')
+                        row.append(host.max_rtt)
+                        table.append(list(row))
+                        row.clear()
                 else:
-                    row.append(address)
-                if host.is_alive:
-                    row.append('Alive')
-                    row.append(str(host.max_rtt) + ' ms')
-                    table.append(list(row))
-                    row.clear()
-                else:
-                    row.append('Down')
-                    row.append(host.max_rtt)
-                    table.append(list(row))
-                    row.clear()
+                    if hostname_resolves(address):
+                        host = ping(address, count = 1, timeout = 1)
+                        row.append(address)
+                        if host.is_alive:
+                            row.append('Alive')
+                            row.append(str(host.max_rtt) + ' ms')
+                            table.append(list(row))
+                            row.clear()
+                        else:
+                            row.append('Down')
+                            row.append(host.max_rtt)
+                            table.append(list(row))
+                            row.clear()
+                    else:
+                        row.append(address)
+                        row.append('DNS query failed for')
+                        row.append('N/A')
+                        table.append(list(row))
+                        row.clear()
             print(tabulate(table, headers=head, tablefmt='grid'))
             table.clear()
             sleep(int(argv[1]))
@@ -55,8 +78,8 @@ if __name__ == '__main__' :
         if len(argv) == 3:
             main()
         else:
-            raise parameterKurang('Parameter kurang')
-    except parameterKurang as parKurang:
+            raise Exception('Parameter kurang')
+    except Exception:
         icmp_help()
-    finally:
-        icmp_help()
+"""    finally:
+        icmp_help()"""
