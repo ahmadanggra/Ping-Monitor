6e0a18f8219133f29d7baf8cbb868735d3b46fa1
Ahmad Anggra <ahmadanggra@gmail.com>
2023-02-17 02:05:46 +0700

    done before add validation fqdn not resolvable

---
+++ icmp_monitor.py
@@ -0,0 +1,62 @@
+from time import sleep
+from sys import argv,exit
+from os import system
+from icmplib import ping,exceptions
+from tabulate import tabulate
+from socket import inet_aton
+from re import match
+
+# usage icmp_monitor <<update in second>> << list of ip address separate by comma (,)
+# example python icmp_monitor.py 192.168.0.1,192.168.0.2
+def icmp_help():  
+    print('Usage:')
+    print('python icmp_monitor.py [update ping in second] [list of ip address/hostname separated by comma.]')
+    exit()    
+
+def main():
+    #hosts = argv[1:]
+    if argv[1] == '--help' or argv[1] == '-h':
+        icmp_help()
+    addresses = list(map(str, argv[-1].split(',')))
+    print(addresses)
+    head = ['Host Address', 'Status', 'RTT']
+    table = list()
+    row = list()
+    try:
+        while(True):
+            system('cls')
+            for address in addresses:
+                host = ping(address, count = 1, timeout = 1)
+                test_ip = match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", address)
+                if test_ip:
+                    row.append(host.address)
+                else:
+                    row.append(address)
+                if host.is_alive:
+                    row.append('Alive')
+                    row.append(str(host.max_rtt) + ' ms')
+                    table.append(list(row))
+                    row.clear()
+                else:
+                    row.append('Down')
+                    row.append(host.max_rtt)
+                    table.append(list(row))
+                    row.clear()
+            print(tabulate(table, headers=head, tablefmt='grid'))
+            table.clear()
+            sleep(int(argv[1]))
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__' :
+    try:
+        
+        if len(argv) == 3:
+            main()
+        else:
+            raise parameterKurang('Parameter kurang')
+    except parameterKurang as parKurang:
+        icmp_help()
+    finally:
+        icmp_help()
