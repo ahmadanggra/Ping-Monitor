c6f48fc386359db60a0fc2ec7975613d7a93b818
ahmad anggra <ahmadanggra@gmail.com>
2025-03-10 16:03:47 +0700

    add smtp_relay support

--- icmp_monitor.py
+++ icmp_monitor.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
 from time import sleep
 from sys import argv,exit
-from os import system
+import os
 from icmplib import ping,exceptions,utils
 from tabulate import tabulate
 from socket import inet_aton
 from re import match
 from socket import gethostbyname,error
+# Used for smtp
+import smtplib
+from email.mime.text import MIMEText
+import threading
 
 # usage icmp_monitor <<update in second>> << list of ip address separate by comma (,)
 # example python icmp_monitor.py 192.168.0.1,192.168.0.2
@@ -21,6 +26,20 @@ def hostname_resolves(hostname):
     except error:
         return 0
 
+def send_email(hostname):
+    subject = "Host " + hostname + " Is Down"
+    body = "Please check connectivity to host " + hostname
+    sender = "ahmadanggra@gmail.com"
+    recipients = ["ahmadanggra@gmail.com"]
+    password = "smqm wwrq tpov nlup"
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+
 def main():
     if argv[1] == '--help' or argv[1] == '-h':
         icmp_help()
@@ -30,10 +49,13 @@ def main():
     row = list()
     try:
         while(True):
-            system('cls')
+            if os.name == 'nt':
+                os.system('cls')
+            else:
+                os.system('clear')
             for address in addresses:              
                 if utils.is_ipv4_address(address):
-                    host = ping(address, count = 1, timeout = 1)
+                    host = ping(address, count = 1, timeout = 1, privileged = False, interval = 0.1)
                     row.append(host.address)
                     if host.is_alive:
                         row.append('Alive')
@@ -42,12 +64,14 @@ def main():
                         row.clear()
                     else:
                         row.append('Down')
-                        row.append(host.max_rtt)
+                        row.append(str(host.max_rtt) + ' ms')
                         table.append(list(row))
                         row.clear()
+                        email_thread = threading.Thread(target=send_email, args=(host.address,))
+                        email_thread.start()
                 else:
                     if hostname_resolves(address):
-                        host = ping(address, count = 1, timeout = 1)
+                        host = ping(address, count = 1, timeout = 1, privileged = False, interval = 0.1)
                         row.append(address)
                         if host.is_alive:
                             row.append('Alive')
@@ -56,9 +80,11 @@ def main():
                             row.clear()
                         else:
                             row.append('Down')
-                            row.append(host.max_rtt)
+                            row.append(str(host.max_rtt) + ' ms')
                             table.append(list(row))
                             row.clear()
+                            email_thread = threading.Thread(target=send_email, args=(host.address,))
+                            email_thread.start()
                     else:
                         row.append(address)
                         row.append('DNS query failed for')
@@ -79,5 +105,6 @@ if __name__ == '__main__' :
             main()
         else:
             raise Exception('Parameter kurang')
-    except Exception:
+    except Exception as e:
+        print(e)
         icmp_help()
